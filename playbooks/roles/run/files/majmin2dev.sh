305e203ba99e9b6e47a3b0d6f261d72ca8f8b100
Jianshen Liu <jliu120@ucsc.edu>
2018-09-23 19:12:31 -0700

    Trace the block I/O events for the test device using ftrace
    Former-commit-id: a0ec55766258e99af74d9e7b5b1e1b8a0a092870
    Former-commit-id: 1228de8413e51888071eb2a410caf689090b550a

---
+++ playbooks/roles/run/files/majmin2dev.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+set -eu -o pipefail
+
+if [ "$#" -ne 1 ]; then
+    cat <<-ENDOFMESSAGE
+Usage: ./majmin2dev.sh DEVICE_NAME
+
+This script converts the device name of a partition to the device ID of its
+parent held by the type dev_t, which can be then used to filter the device in
+the block:* events in ftrace.
+
+DEVICE_NAME:
+    Note that this is the device name of a partition.
+
+For example:
+    ./majmin2dev.sh /dev/sda1
+
+See
+https://linux.die.net/man/3/minor
+ENDOFMESSAGE
+    exit
+fi
+
+device_name="$1"
+
+pdevice_name=/dev/"$(lsblk --noheadings --output pkname "$device_name" | tail -1)"
+major="$(stat --format '%t' "$pdevice_name")"
+minor="$(stat --format '%T' "$pdevice_name")"
+
+echo "$pdevice_name"
+
+# See how to convert the major and minor numbers to the device ID:
+#   https://github.com/brendangregg/perf-tools/blob/master/iolatency
+echo "$(( (major << 20) + minor ))"
